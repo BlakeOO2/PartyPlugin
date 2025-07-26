8d42feffb0811bc28ddb4c43886c053e2fbb4455
BlakeOO2 <46853784+BlakeOO2@users.noreply.github.com>
2025-07-25 20:01:52 -0600

    fixed PartyAPIImplementation

--- src/main/java/com/Blakeoo2/PartyAPI/PartyAPIImplementation.java
+++ src/main/java/com/Blakeoo2/PartyAPI/PartyAPIImplementation.java
@@ -1,10 +1,12 @@
 package com.Blakeoo2.PartyAPI;
 
+import com.Blakeoo2.PartyAPI.Chat.ChatAPI;
+import com.Blakeoo2.PartyAPI.Chat.ChatImpl;
 import com.Blakeoo2.PartyAPI.Members.MembersAPI;
 import com.Blakeoo2.PartyAPI.Members.MembersImpl;
 import com.Blakeoo2.party.PartyManager;
 
-public class PartyAPIImplementation {
+public class PartyAPIImplementation implements PartyAPI{
 
     private final PartyManager partyManager;
 
@@ -13,9 +15,12 @@ public class PartyAPIImplementation {
     }
 
 
+    @Override
+    public ChatAPI chat() {
+        return new ChatImpl();
+    }
+
     public MembersAPI members(){
         return new MembersImpl();
     }
-    //ChatAPI chat();
-    //MembersAPI members();
 }
